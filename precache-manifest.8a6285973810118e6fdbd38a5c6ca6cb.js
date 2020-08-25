79771f7169ff4f01d39f255f6f3bb83f8a6f5205
oluwatunbi <davies899@yahoo.com>
2020-08-25 22:49:23 +0100

    Updates

---
+++ precache-manifest.8a6285973810118e6fdbd38a5c6ca6cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2383fc716096b922db3",
+    "url": "/Robotselect/static/css/main.8ab90954.chunk.css"
+  },
+  {
+    "revision": "d2383fc716096b922db3",
+    "url": "/Robotselect/static/js/main.da070cb4.chunk.js"
+  },
+  {
+    "revision": "027b9fb7016289b160de",
+    "url": "/Robotselect/static/js/runtime~main.2dbc18da.js"
+  },
+  {
+    "revision": "fd742d400dd87f5a902a",
+    "url": "/Robotselect/static/css/2.7fb21b15.chunk.css"
+  },
+  {
+    "revision": "fd742d400dd87f5a902a",
+    "url": "/Robotselect/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robotselect/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "25ff1688d3f60e682d5c6e58a50de817",
+    "url": "/Robotselect/index.html"
+  }
+];
